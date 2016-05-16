de71be2a70d85b7680c42ced3cd8c26bd9a28334
malkio <coding.malkio@gmail.com>
2016-05-16 18:02:14 +0800

    Add necessary files

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,3 +1,17 @@
 (function($, window, document, undefined){
+	var source = "data.json";
+	console.log(url);
+	$.ajax({
+		url: source,
+		cache: false,
+		success: function(data){
+			initialize(data);
+		}
+	});
 
+
+
+	function initialize(data){
+		
+	}
 }(jQuery, window, document, undefined));
